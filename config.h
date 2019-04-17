f93089de1384b1060e9b56a02eb452b96e8d182a
rodrigoumanzor1 <49285399+rodrigoumanzor1@users.noreply.github.com>
2019-04-17 00:00:26 -0700

    Create config.h

---
+++ config.h
@@ -0,0 +1,22 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "rodrigoumanzor"
+#define IO_KEY         "55575ec8c52b4f9ea65554e909e8f4cc"
+
+/******************************* WIFI **************************************/
+
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather HUZZAH ESP32 -> https://www.adafruit.com/product/3405
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID       "STC2"
+#define WIFI_PASS       "bahtsang"
+
+// comment out the following two lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
